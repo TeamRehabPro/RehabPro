2334358a56a0d40e45d5e350ecfb7e0277ef30dd
Jinwhl <103013505+Jinwhl@users.noreply.github.com>
2024-08-06 11:42:08 +0900

    Screen split

--- app/src/main/java/com/rehabilitationpro/MainActivity.kt
+++ app/src/main/java/com/rehabilitationpro/MainActivity.kt
@@ -3,23 +3,66 @@ package com.rehabilitationpro
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.rehabilitationpro.screen.login.LoginScreen
+import com.rehabilitationpro.screen.notice.NoticeScreen
+import com.rehabilitationpro.screen.schedule.ScheduleScreen
 import com.rehabilitationpro.ui.theme.RehabPROTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             RehabPROTheme {
-                Text("this is Rehab PRO")
+                val navController = rememberNavController()
+                AppNavHost(navController = navController)
             }
+        }
+    }
+}
+
+@Composable
+fun AppNavHost(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "login") {
+        composable("login") { LoginScreen(navController) }
+
+        composable("main") { MainMenu(navController) }
+        composable("notice") { NoticeScreen(navController) }
+        composable("schedule") { ScheduleScreen(navController) }
+    }
+}
+
+@Composable
+fun MainMenu(navController: NavHostController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = { navController.navigate("notice") },
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Text(text = "Go to Notice")
+        }
+        Button(
+            onClick = { navController.navigate("schedule") },
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Text(text = "Go to Schedule")
         }
     }
 }
