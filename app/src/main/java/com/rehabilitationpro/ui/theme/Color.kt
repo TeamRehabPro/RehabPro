ede5ea6b90a9ba72cbf4c7c162f8bf95c7042eb3
Jinwhl <103013505+Jinwhl@users.noreply.github.com>
2024-08-08 12:32:10 +0900

    UI - Color Added

--- app/src/main/java/com/rehabilitationpro/ui/theme/Color.kt
+++ app/src/main/java/com/rehabilitationpro/ui/theme/Color.kt
@@ -9,3 +9,29 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+data class ColorPalette(
+    val Gray50: Color = Color(0xFFF8FAFC),
+    val Gray100: Color = Color(0xFFF1F5F9),
+    val Gray200: Color = Color(0xFFE2E8F0),
+    val Gray300: Color = Color(0xFFCBD5E1),
+    val Gray400: Color = Color(0xFF94A3B8),
+    val Gray500: Color = Color(0xFF64748B),
+    val Gray600: Color = Color(0xFF475569),
+    val Gray700: Color = Color(0xFF334155),
+    val Gray800: Color = Color(0xFF1E293B),
+    val Gray900: Color = Color(0xFF0F172A),
+
+    val Gray: Color = Color(0xFFBBBBBB),
+    val White: Color = Color(0xFFFFFFFF),
+    val Purple: Color = Color(0xFF692BE0),
+    val Mint: Color = Color(0xFF5FD4C5),
+    val SkyBlue: Color = Color(0xFF6380E5),
+    val Blue: Color = Color(0xFF403BBD),
+    val DarkBlue: Color = Color(0xFF160E6F),
+    val Black: Color = Color(0xFF000000)
+)
+
+//// Usage example
+//val colors = ColorPalette()
+//val primaryColor = colors.Purple
