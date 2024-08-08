362fdd024ea10bcbf909c9311c9bcac0c5603f52
Jinwhl <103013505+Jinwhl@users.noreply.github.com>
2024-08-08 22:54:35 +0900

    Reservation Screen Updated

--- app/src/main/java/com/rehabilitationpro/screens/menus/reservation/ReservationMainScreen.kt
+++ app/src/main/java/com/rehabilitationpro/screens/menus/reservation/ReservationMainScreen.kt
@@ -2,92 +2,63 @@ package com.rehabilitationpro.screens.menus.reservation
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.DrawerState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.rehabilitationpro.Screen
+import com.rehabilitationpro.bar.TopBar
+import com.rehabilitationpro.screens.menus.reservation.component.CalendarView
 import java.time.LocalDate
 import java.time.YearMonth
-import java.time.format.TextStyle
-import java.util.Locale
 
-@OptIn(ExperimentalMaterial3Api::class)
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun ReservationMainScreen(navController: NavHostController) {
-    val currentMonth = YearMonth.now()
+fun ReservationMainScreen(navController: NavHostController, drawerState: DrawerState) {
     val currentDate = LocalDate.now()
+    val currentMonth = remember { mutableStateOf(YearMonth.now()) }
+    val isYearView = remember { mutableStateOf(false) }
 
-    // Sample reservations for today
-    val todayReservations =
-        listOf("Patient A - 10:00 AM", "Patient B - 11:30 AM", "Patient C - 1:00 PM")
+    val currentRoute = Screen.ReservationScreen.Main.route
 
     Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text("Main Menu") },
-                navigationIcon = {
-                    IconButton(onClick = {
-                        // Navigate back to the Login screen
-                        navController.navigate(Screen.MainMenu.route)
-                    }) {
-                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
-                    }
-                }
-            )
-        }
+        topBar = { TopBar(navController, drawerState, currentRoute) },
     ) { paddingValues ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+                .padding(16.dp)
         ) {
             // Display the current month's calendar
-            Text(
-                text = "${
-                    currentMonth.month.getDisplayName(
-                        TextStyle.FULL,
-                        Locale.getDefault()
-                    )
-                } ${currentMonth.year}",
-                style = MaterialTheme.typography.bodyLarge,
-                fontWeight = FontWeight.Bold,
-                fontSize = 24.sp,
-                modifier = Modifier.padding(bottom = 16.dp)
+            CalendarView(
+                currentMonth = currentMonth.value,
+                currentDate = currentDate,
+                onMonthChange = { newMonth ->
+                    currentMonth.value = newMonth
+                },
+                onTodayClick = {
+                    currentMonth.value = YearMonth.now()
+                },
+                isYearView = isYearView,
+                onYearChange = { newYear ->
+                    currentMonth.value = YearMonth.of(newYear, currentMonth.value.month)
+                }
             )
-
-            CalendarView(currentMonth, currentDate)
 
             Spacer(modifier = Modifier.height(16.dp))
 
@@ -99,6 +70,9 @@ fun ReservationMainScreen(navController: NavHostController) {
                 modifier = Modifier.padding(bottom = 8.dp)
             )
 
+            // Sample reservations for today
+            val todayReservations =
+                listOf("Patient A - 10:00 AM", "Patient B - 11:30 AM", "Patient C - 1:00 PM")
             LazyColumn {
                 items(todayReservations) { reservation ->
                     Text(
@@ -110,55 +84,6 @@ fun ReservationMainScreen(navController: NavHostController) {
             }
 
             Spacer(modifier = Modifier.height(16.dp))
-        }
-    }
-}
-
-@RequiresApi(Build.VERSION_CODES.O)
-@Composable
-fun CalendarView(currentMonth: YearMonth, currentDate: LocalDate) {
-    val daysInMonth = currentMonth.lengthOfMonth()
-    val firstDayOfMonth = currentMonth.atDay(1).dayOfWeek.value % 7
-
-    Column {
-        // Days of the week header
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceAround
-        ) {
-            listOf("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat").forEach { day ->
-                Text(
-                    text = day,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 16.sp
-                )
-            }
-        }
-
-        // Calendar days
-        var dayOfMonth = 1 - firstDayOfMonth
-        for (week in 0 until 6) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceAround
-            ) {
-                for (day in 0..6) {
-                    if (dayOfMonth in 1..daysInMonth) {
-                        Box(
-                            modifier = Modifier
-                                .size(40.dp)
-                                .background(if (dayOfMonth == currentDate.dayOfMonth) Color.LightGray else Color.Transparent)
-                                .clickable { /* Handle day click */ },
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text(text = dayOfMonth.toString())
-                        }
-                    } else {
-                        Box(modifier = Modifier.size(40.dp))
-                    }
-                    dayOfMonth++
-                }
-            }
         }
     }
 }
