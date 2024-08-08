2f79c20757d69214dcb1878a90a8d35255423756
Jinwhl <103013505+Jinwhl@users.noreply.github.com>
2024-08-08 22:56:45 +0900

    Messenger Screen Updated

--- app/src/main/java/com/rehabilitationpro/screens/menus/messenger/MessengerMainScreen.kt
+++ app/src/main/java/com/rehabilitationpro/screens/menus/messenger/MessengerMainScreen.kt
@@ -4,45 +4,28 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.DrawerState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.rehabilitationpro.Screen
+import com.rehabilitationpro.bar.TopBar
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MessengerMainScreen(navController: NavHostController) {
+fun MessengerMainScreen(navController: NavHostController, drawerState: DrawerState) {
+    val currentRoute = Screen.MessengerScreen.Main.route
+
     Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text("Main Menu") },
-                navigationIcon = {
-                    IconButton(onClick = {
-                        // Navigate back to the Login screen
-                        navController.navigate(Screen.MainMenu.route)
-                    }) {
-                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
-                    }
-                }
-            )
-        }
-    ) { paddingValues ->
+        topBar = { TopBar(navController, drawerState, currentRoute) },
+    ) { innerPadding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(paddingValues)
-                .padding(16.dp),
+                .padding(innerPadding),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
