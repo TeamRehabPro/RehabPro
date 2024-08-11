aa1f8f49c80a57c89d909e7c01dee6839a2c9354
Jinwhl <103013505+Jinwhl@users.noreply.github.com>
2024-08-12 03:10:21 +0900

    자주 쓰이는 위젯 - CustomInputField : 함수의 사용성과 빠른 디자인 반영을 위해 TextField -> BasicTextField, Box 사용하는 방식으로 변경

--- app/src/main/java/com/rehabilitationpro/widgets/CustomInputField.kt
+++ app/src/main/java/com/rehabilitationpro/widgets/CustomInputField.kt
@@ -1,56 +1,70 @@
 package com.rehabilitationpro.widgets
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
+import com.rehabilitationpro.R
 import com.rehabilitationpro.ui.theme.ColorPalette
 
 @Composable
-fun EmailInputField(
-    state: MutableState<String>,
+fun NameInputField(
+    fieldValue: MutableState<String>,
     modifier: Modifier = Modifier
 ) {
     CustomInputField(
-        state = state,
+        fieldValue = fieldValue,
+        placeholder = "Enter your name",
+        iconResId = R.drawable.icon_user,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun EmailInputField(
+    fieldValue: MutableState<String>,
+    modifier: Modifier = Modifier
+) {
+    CustomInputField(
+        fieldValue = fieldValue,
         placeholder = "Enter your email",
-        icon = Icons.Default.Email,
+        iconResId = R.drawable.icon_email,
         modifier = modifier
     )
 }
 
 @Composable
 fun PasswordInputField(
-    state: MutableState<String>,
+    fieldValue: MutableState<String>,
     modifier: Modifier = Modifier
 ) {
     CustomInputField(
-        state = state,
+        fieldValue = fieldValue,
         placeholder = "Enter your password",
-        icon = Icons.Default.Lock,
+        iconResId = R.drawable.icon_lock,
         isPassword = true,
         modifier = modifier
     )
@@ -59,54 +73,62 @@ fun PasswordInputField(
 /**
  * A customizable input field component for text input.
  *
- * @param state The mutable state holding the current input value.
+ * @param fieldValue The mutable state holding the current input value.
  * @param placeholder The placeholder text to display when the input is empty.
- * @param icon The icon to display at the start of the input field.
+ * @param iconResId The icon to display at the start of the input field.
  * @param isPassword Whether this field is for password input. If true, the input will be masked.
  * @param modifier Modifier to be applied to the input field.
  */
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CustomInputField(
-    state: MutableState<String>,
+    fieldValue: MutableState<String>,
     placeholder: String,
-    icon: ImageVector,
+    iconResId: Int,
     isPassword: Boolean = false,
     @SuppressLint("ModifierParameter") modifier: Modifier = Modifier
 ) {
-    Row(
-        modifier = modifier
-            .background(color = ColorPalette.inputBoxGray)
-            .fillMaxWidth(0.8f)
-            .height(56.dp)
-            .background(Color.White)
-            .border(1.dp, ColorPalette.borderGray, shape = RoundedCornerShape(12.dp))
-    ) {
-        Icon(
-            imageVector = icon,
-            contentDescription = null,
-            tint = ColorPalette.textGray,
-            modifier = Modifier
-                .padding(start = 24.dp, end = 8.dp)
-                .fillMaxHeight()
-        )
-        TextField(
-            value = state.value,
-            onValueChange = { state.value = it },
-            placeholder = {
-                Text(
-                    text = placeholder,
-                    color = ColorPalette.textGray
-                )
-            },
-            visualTransformation = if (isPassword) PasswordVisualTransformation() else VisualTransformation.None,
-            keyboardOptions = if (isPassword) KeyboardOptions(keyboardType = KeyboardType.Password) else KeyboardOptions.Default,
+    val borderColor = if (fieldValue.value.isEmpty()) ColorPalette.borderGray else ColorPalette.signInBlue
 
-            colors = TextFieldDefaults.outlinedTextFieldColors(
-                focusedBorderColor = Color.Transparent,
-                unfocusedBorderColor = Color.Transparent,
-                cursorColor = ColorPalette.signInBlue,
-            ),
-        )
+    Surface(
+        modifier = modifier
+            .fillMaxWidth(0.8f)
+            .height(56.dp),
+        shape = RoundedCornerShape(12.dp),
+        color = ColorPalette.inputBoxGray,
+        border = BorderStroke(1.dp, borderColor)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = iconResId),
+                contentDescription = null,
+                tint = ColorPalette.textGray,
+                modifier = Modifier.size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            BasicTextField(
+                value = fieldValue.value,
+                onValueChange = {
+                    fieldValue.value = it
+                },
+                textStyle = TextStyle(color = Color.Black),
+                decorationBox = { innerTextField ->
+                    Box {
+                        if (fieldValue.value.isEmpty()) {
+                            Text(
+                                text = placeholder,
+                                color = ColorPalette.textGray
+                            )
+                        }
+                        innerTextField()
+                    }
+                },
+                visualTransformation = if (isPassword) PasswordVisualTransformation() else VisualTransformation.None,
+                keyboardOptions = if (isPassword) KeyboardOptions(keyboardType = KeyboardType.Password) else KeyboardOptions.Default,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
     }
 }
