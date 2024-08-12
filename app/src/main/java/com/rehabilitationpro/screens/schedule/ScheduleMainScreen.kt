c80b84eea038049b48d0433b66f67225f5739d8d
Jinwhl <103013505+Jinwhl@users.noreply.github.com>
2024-08-12 16:54:31 +0900

    SettingScreen, ScheduleMainScreen 빈 껍데기 화면 추가

---
+++ app/src/main/java/com/rehabilitationpro/screens/schedule/ScheduleMainScreen.kt
@@ -0,0 +1,10 @@
+package com.rehabilitationpro.screens.schedule
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun ScheduleMainScreen(navHostController: NavHostController) {
+    Text("스케줄")
+}
---
+++ app/src/main/java/com/rehabilitationpro/screens/setting/SettingScreen.kt
@@ -0,0 +1,10 @@
+package com.rehabilitationpro.screens.setting
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun SettingScreen(navHostController: NavHostController) {
+    Text("세팅")
+}
