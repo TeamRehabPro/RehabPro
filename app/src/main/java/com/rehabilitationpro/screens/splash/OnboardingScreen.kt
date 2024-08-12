cbe7c86ea084699fc7a316fa90961c370c67c158
Jinwhl <103013505+Jinwhl@users.noreply.github.com>
2024-08-12 21:32:42 +0900

    시작 화면 이미지 사용성을 위한 코드 분리

--- app/src/main/java/com/rehabilitationpro/screens/splash/OnboardingScreen.kt
+++ app/src/main/java/com/rehabilitationpro/screens/splash/OnboardingScreen.kt
@@ -1,21 +1,17 @@
-package com.rehabilitationpro.screens
+package com.rehabilitationpro.screens.splash
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import com.rehabilitationpro.R
 import com.rehabilitationpro.Screen
+import com.rehabilitationpro.screens.splash.component.StartScreenImage
 import com.rehabilitationpro.widgets.GoToSignInScreenButton
 import com.rehabilitationpro.widgets.GoToSignUpScreenButton
 
@@ -28,13 +24,8 @@ fun OnboardingScreen(navController: NavHostController) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         // 첫 실행 화면 로고 이미지
-        Image(
-            painter = painterResource(id = R.drawable.doctor), // 이미지 경로 : res/drawable 에서 불러올 수 있음.
-            modifier = Modifier
-                .fillMaxHeight(0.64f),  // 이미지 전체 크기의 64%
-            contentScale = ContentScale.FillHeight,
-            contentDescription = null,
-        )
+        StartScreenImage()
+        Spacer(modifier = Modifier.height(24.dp))
 
         // Sign In Screen 전환 Button (로그인 버튼)
         GoToSignInScreenButton(onClick = { navController.navigate(Screen.SignIn.route) })
